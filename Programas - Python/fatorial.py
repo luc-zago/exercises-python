1e0e025a37e4f312f3cab2a7f2bb50306861d627
Lucas <luc.zago@hotmail.com>
2021-07-09 11:42:48 -0300

    Atualizando exercícios

--- Programas - Python/fatorial.py
+++ Programas - Python/fatorial.py
@@ -1,12 +1,18 @@
 
-print("Calcula o fatorial de um número.")
-f=int(input("Digite um número inteiro não negativo: "))
+# print("Calcula o fatorial de um número.")
+# f = int(input("Digite um número inteiro não negativo: "))
 
-c=1
-fat=1
+# c = 1
+# fat = 1
 
-while c<=f:
- fat=fat*c
- c=1+c
+# while c <= f:
+# fat = fat * c
+# c = 1 + c
 
-print(fat)
+# print(fat)
+
+def fatorial(n):
+    if n < 1:
+        return 1
+    else:
+        return n * fatorial(n - 1)
--- Programas - Python/soma_elementos.py
+++ Programas - Python/soma_elementos.py
@@ -9,3 +9,19 @@ def soma_elementos(n):
         z=z+n[y]
         y=y+1
     return z
+
+def soma_lista1(lista, soma=0, index=None):
+    if index == None:
+        index = len(lista) - 1
+    soma = soma + lista[index]
+    index = index - 1
+    if index == -1:
+        return soma
+    return soma_lista(lista, soma, index)
+
+def soma_lista(lista):
+    if len(lista) == 1:
+        return lista[0]
+    lista2 = lista[:len(lista) - 1]
+    lista2[len(lista2) -1] += lista[len(lista) - 1]
+    return soma_lista(lista2)
